bd548435302f26b93ca422a8b0c84bce263dec3c
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-02-21 15:55:03 +0600

    create for and ul in js

--- script.js
+++ script.js
@@ -40,3 +40,8 @@ example1.append(example7);
 
 let example8 = document.createElement("ul");
 document.body.append(example8);
+for( let i =1; i <=10; i ++) {
+  let listItem = document.createElement("li");
+  listItem.textContent = "example " + i;
+  example8.append(listItem);
+}
